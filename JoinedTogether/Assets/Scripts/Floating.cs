aa4408ad87bfd56e8ce78da2b086faec7ea06264
JaesonD2 <JaesonD2@hotmail.com>
2021-06-12 17:34:39 +1000

    Updated Lighting of first level
    Added a lot

--- JoinedTogether/Assets/Scripts/Floating.cs
+++ JoinedTogether/Assets/Scripts/Floating.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Floating : MonoBehaviour
 {
+    public float amplitude = 0.2f;
+    public float frequency = 0.5f;
+
+    Vector2 startPos;
+    Vector2 tempPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tempPos = startPos;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
     }
 }
