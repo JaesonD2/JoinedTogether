f59fb941ce4f6f9b46458c0e7a4d44637fef24af
JaesonD2 <JaesonD2@hotmail.com>
2021-06-12 16:32:57 +1000

    Made Various Win things
    Particles etc

--- JoinedTogether/Assets/Scripts/CharacterSelector.cs
+++ JoinedTogether/Assets/Scripts/CharacterSelector.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CharacterSelector : MonoBehaviour
 {
-    public Rigidbody2D blueRB;
+    public Rigidbody2D cyanRB;
+    public Rigidbody2D yellowRB;
+    public Rigidbody2D magentaRB;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.name == "BlueSelector")
+        if (collision.name == "CyanSelector")
         {
-            blueRB.constraints = RigidbodyConstraints2D.FreezeRotation;
+            cyanRB.constraints = RigidbodyConstraints2D.FreezeRotation;
+            yellowRB.constraints = RigidbodyConstraints2D.FreezeAll;
+            magentaRB.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        if (collision.name == "MagentaSelector")
+        {
+            magentaRB.constraints = RigidbodyConstraints2D.FreezeRotation;
+            yellowRB.constraints = RigidbodyConstraints2D.FreezeAll;
+            cyanRB.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        if (collision.name == "YellowSelector")
+        {
+            yellowRB.constraints = RigidbodyConstraints2D.FreezeRotation;
+            magentaRB.constraints = RigidbodyConstraints2D.FreezeAll;
+            cyanRB.constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
 
