ce96ee35bcb7ed857a588b61872b73f1ba8507ca
JaesonD2 <JaesonD2@hotmail.com>
2021-06-12 12:57:45 +1000

    Death and Platform
    Player movement
    Scene restarts when touches red
    can move on platform

---
+++ JoinedTogether/Assets/Scripts/RestartScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided with " + collision.name + collision.gameObject.layer);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            //Restarts the level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
