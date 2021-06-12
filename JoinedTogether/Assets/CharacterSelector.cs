c2a95c91e60a34c9de15500b5ad5a6172eb5892e
JaesonD2 <JaesonD2@hotmail.com>
2021-06-12 15:01:27 +1000

    Basic Game in
    Three levels
    player and second player working

---
+++ JoinedTogether/Assets/CharacterSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public Rigidbody2D blueRB;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "BlueSelector")
+        {
+            blueRB.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+
+}
---
+++ JoinedTogether/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public SpriteRenderer fader;
+    public float fadeSpeed = 10f;
+
+    public void Fade(bool fadeToBlack = true)
+    {
+        StartCoroutine(FadeBlackOutSquare(fadeToBlack));
+    }
+
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true)
+    {
+        Color objectColor = fader.color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            // While not quite black
+            while (fader.color.a < 1)
+            {
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, objectColor.a + (fadeSpeed * Time.deltaTime));
+                fader.color = objectColor;
+                yield return null;
+            }
+        }
+        else
+        {
+            // While not quite invisible
+            while (fader.color.a > 0)
+            {
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, objectColor.a - (fadeSpeed * Time.deltaTime));
+                fader.color = objectColor;
+                yield return null;
+            }
+        }
+    }
+}
